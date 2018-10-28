e830d82a52daee4211454a5ee1e9ffbefc1197b8
GiovanniVanAssche <34142722+GiovanniVanAssche@users.noreply.github.com>
2018-10-28 23:49:51 +0100

    Create name.js

---
+++ name.js
@@ -0,0 +1,6 @@
+var local_user_index_array = sessionStorage.getItem("UserIndex");    
+         
+User_local_Name = sessionStorage.getItem("UserName");      
+
+// Verwelkoming   
+document.getElementById("header_username").innerHTML = User_local_Name;   
